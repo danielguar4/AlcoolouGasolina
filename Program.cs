b625103424440668f0e735b4598794716b0d44f5
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-06-05 18:52:11 -0300

    terminando

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿double etanol, gasolina, Calculo = 0;
+
+Console.WriteLine(@"/////  Preço da Gasolina e do Alcool  \\\\\");
+
+Console.WriteLine("Preço do Etanol...: ");
+etanol = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Preço do Gasolina...: ");
+gasolina = Convert.ToDouble(Console.ReadLine());
+
+void Calculo_Preco(){
+    Calculo = etanol / gasolina;
+
+    if(Calculo > 0.73){
+        Console.WriteLine("Abasteça com Gasolina Mais em Conta");
+        
+    }
+}
+
+Calculo_Preco();
+
+Console.WriteLine($"{Calculo}");
